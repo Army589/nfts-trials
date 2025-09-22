73f0585c017cf1a7a4ad1a3ad08674438eaedca1
XRP Army CTO <thearmy589@gmail.com>
2025-09-22 13:18:11 +0200

    Update hello.js
    debugging hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,3 @@
-export default async function handler(req, res) {
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: { "content-type": "application/json" },
-  });
+export default function handler(req, res) {
+  res.status(200).json({ ok: true });
 }
